6038628be59e3c00793e3671242ed3cd3d1d1b9e
Abdul Hannan Ahmed <133767980+Hannannahmed@users.noreply.github.com>
2026-01-30 06:29:14 +0000

    Update Isues

--- services/plant.service.ts
+++ services/plant.service.ts
@@ -1,7 +1,7 @@
 import axiosInterceptor from "@/lib/axiosInterceptor"
 
 
-/* GET ALL PLANTS */
+
 export const getPlants = async () => {
   const res = await axiosInterceptor.get("/plants")
   return res.data.data.plants
